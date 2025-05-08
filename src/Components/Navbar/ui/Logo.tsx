b0ce6e980c6fd6dab0810375a5ec808e1b0aaecb
atis02 <guwancikramow@gmail.com>
2025-05-08 18:24:23 +0500

    category subcategory segment effects

--- src/Components/Navbar/ui/Logo.tsx
+++ src/Components/Navbar/ui/Logo.tsx
@@ -1,30 +1,18 @@
 import { Typography } from "@mui/material";
-import { FC, useState } from "react";
-import { useSelector } from "react-redux";
+import { FC } from "react";
 import { Link } from "react-router-dom";
-import { mainColor } from "../../utils/CustomStyles";
 interface Props {
   isMobile?: boolean;
 }
 const Logo: FC<Props> = ({ isMobile }) => {
-  const [isHovered, setisHovered] = useState(false);
-  const currentSlide = useSelector((state: any) => state.swiper.color);
-  const currentTextColor = useSelector(
-    (state: any) => state.swiper.colorNavbarText
-  );
   return (
     <Typography variant="h6" component="div" sx={{ flexGrow: 0.25 }}>
-      <Link
-        to="/"
-        onMouseEnter={() => setisHovered(true)}
-        onMouseLeave={() => setisHovered(false)}
-        style={{ textDecoration: "none", color: "inherit" }}
-      >
+      <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
         <img
           src="/logos/LogoMain.png"
           style={{
-            width: "50px",
-            height: "50px",
+            width: isMobile ? "40px" : "50px",
+            height: isMobile ? "40px" : "50px",
           }}
           alt="LogoMain"
         />
